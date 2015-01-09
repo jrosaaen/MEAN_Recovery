7ebd38818df5f8fcee413c6d1f825f6424a59813
Joshua Rosaaen <joshua.rosaaen@gmail.com>
2015-01-08 19:35:29 -0800

    modify the facilities schema

--- app/models/facility.server.model.js
+++ app/models/facility.server.model.js
@@ -4,26 +4,66 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 /**
  * Facility Schema
  */
 var FacilitySchema = new Schema({
-	name: {
-		type: String,
-		default: '',
-		required: 'Please fill Facility name',
-		trim: true
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+    // name: {
+    // 	type: String,
+    // 	default: '',
+    // 	required: 'Please fill Facility name',
+    // 	trim: true
+    // },
+    facilityName: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    facilityStreetAddress: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    facilityStreetAddress2: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    facilityCity: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    facilityState: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    facilityZip: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    serviceSupported: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    facilityRequireCertification: {
+        type: Boolean,
+        default: '',
+        trim: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
 });
 
 mongoose.model('Facility', FacilitySchema);
